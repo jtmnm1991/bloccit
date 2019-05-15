e566365f2952469c6c8af9d35061c88b0ca746c2
EC2 Default User <ec2-user@ip-172-31-12-176.us-east-2.compute.internal>
2019-05-15 19:59:12 +0000

    assignment-16-rails-seeding-data: correction

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,30 +1,25 @@
 require 'random_data'
 
- # Create Posts
- 50.times do
- # #1
-   Post.create!(
- # #2
-     title:  RandomData.random_sentence,
-     body:   RandomData.random_paragraph
-   )
- end
- posts = Post.all
- 
- # Create Comments
- # #3
- 100.times do
-   Comment.create!(
- # #4
-     post: posts.sample,
-     body: RandomData.random_paragraph
-   )
- end
- 
- puts "#{Post.count}"
- Post.find_or_create_by(title: "rando title", body: "rando body")
- puts "#{Post.count}"
- 
- puts "Seed finished"
- puts "#{Post.count} posts created"
- puts "#{Comment.count} comments created"
+50.times do
+    Post.create!(
+        title: RandomData.random_sentence,
+        body: RandomData.random_paragraph
+    )
+end
+
+posts = Post.all
+
+100.times do
+    Comment.create!(
+        post: posts.sample,
+        body: RandomData.random_paragraph
+    )
+end
+
+Post.find_or_create_by(title:"The Title of Titles", body:"A Body of Bodies")
+
+Comment.find_or_create_by(post: posts.first, body:"The Unique of the Unique")
+
+puts "Seed finished"
+puts "#{Post.count} posts created"
+puts "#{Comment.count} comments created"
