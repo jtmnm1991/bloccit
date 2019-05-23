b2b0508df26890932a866c780b29daaec5b4e906
EC2 Default User <ec2-user@ip-172-31-12-176.us-east-2.compute.internal>
2019-05-23 21:05:26 +0000

    assignment 18 rails crud: correction

--- db/migrate/20190515214555_create_advertisements.rb
+++ db/migrate/20190515214555_create_advertisements.rb
@@ -2,7 +2,7 @@ class CreateAdvertisements < ActiveRecord::Migration[5.1]
   def change
     create_table :advertisements do |t|
       t.string :title
-      t.text :copy
+      t.text :body
       t.integer :price
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,7 @@ ActiveRecord::Schema.define(version: 20190515214555) do
 
   create_table "advertisements", force: :cascade do |t|
     t.string "title"
-    t.text "copy"
+    t.text "body"
     t.integer "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
